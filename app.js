35d23a79aca46d1b5facf0827467ac9363605d7b
trupti0406 <truptivijayyadav27@gmail.com>
2022-07-12 01:06:52 +0530

    Q and A completed

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // 1. Using selectors inside the element
+const questions = document.querySelectorAll(".question");
+questions.forEach(function (question) {
+  //   console.log(question);
+  const btn = question.querySelector(".question-btn");
+  //   console.log(btn);
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
 
-
-
-
-
+    question.classList.toggle("show-text");
+  });
+});
 
 // 2. Traversing The DOM
 // const buttons = document.querySelectorAll(".question-btn");
