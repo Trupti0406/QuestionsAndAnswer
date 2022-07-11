55f2cb7e10f746a46ea23cc1c30058c2ef5a7306
trupti0406 <truptivijayyadav27@gmail.com>
2022-07-11 22:58:08 +0530

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 //using selectors inside the element
 // traversing the dom
+
+const buttons = document.querySelectorAll(".question-btn")
+
+buttons.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+        
+    })
+})
+
